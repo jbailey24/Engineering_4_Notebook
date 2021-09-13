78613865d15620c7656bc76f56388af22faf7b2d
jbailey24 <56133021+jbailey24@users.noreply.github.com>
2021-09-13 12:50:57 -0400

    This worked in the emulator but not on the pi

---
+++ Python/dice_roller.py
@@ -0,0 +1,36 @@
+# Automatic Dice Roller
+# Written by Julia Bailey
+
+from random import randint
+
+sides = 6
+dice = 1
+
+print("Automatic Dice Roller")
+print("Press Enter to roll")
+print("Enter x to quit")
+imp = input("Enter c to customize")
+    
+while True:
+    
+    if imp == "x":
+        break
+    
+    if imp == 'c':
+        print("Enter s to change number of sides")
+        imp2 = input("Enter d to change number of dice")
+        
+        if imp2 == "s":
+            sides = input("How many sides?")
+            imp = 'back'
+        if imp2 == "d":
+            dice = input("How many dice?")
+            imp = 'back'
+        
+    else:
+        for x in range(int(dice)):
+            print((randint(1,int(sides))))
+        
+        print("Enter c for options")
+        
+    print()
