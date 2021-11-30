d0d69b5957928e34e5f80f33f50c30532d55fa3e
jbailey24 <56133021+jbailey24@users.noreply.github.com>
2021-11-30 11:47:14 -0500

    Update I2C_Practice.py

--- Python/I2C_Practice.py
+++ Python/I2C_Practice.py
@@ -1,5 +1,10 @@
+import time
+
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
+import Adafruit_LSM303
+
+lsm303 = Adafruit_LSM303.LSM303()
 
 from PIL import Image
 from PIL import ImageDraw
@@ -31,9 +36,20 @@ x = padding
 
 font = ImageFont.load_default()
 
-draw.text((x, top),    'Hello',  font=font, fill=255)
-draw.text((x, top+20), 'World!', font=font, fill=255)
+draw.text((x, top), 'ACCEL DATA:', font=font, fill=255)
 
-# Display image.
-disp.image(image)
-disp.display()
+while True:
+  
+  accel, mag = lsm303.read()
+  accel_x, accel_y, accel_z = accel
+  mag_x, mag_y, mag_z = mag
+
+  draw.text((x, top+12),    'X: '+accel_x,  font=font, fill=255)
+  draw.text((x, top+24), 'Y: '+accel_y, font=font, fill=255)
+  draw.text((x, top+36), 'Z: '+accel_z, font=font, fill=255)
+
+  disp.image(image)
+  disp.display()
+
+  time.sleep(1)
+
