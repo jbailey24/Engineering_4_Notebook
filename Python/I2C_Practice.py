da822fc479ec46bd7e9cb085684645d047f9aa17
jbailey24 <56133021+jbailey24@users.noreply.github.com>
2021-11-30 11:33:40 -0500

    Create I2C_Practice.py

---
+++ Python/I2C_Practice.py
@@ -0,0 +1,34 @@
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+RST = 24
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
+
+disp.begin()
+
+disp.clear()
+disp.display()
+
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+draw = ImageDraw.Draw(image)
+
+font = ImageFont.load_default()
+
+draw.text((x, top),    'Hello',  font=font, fill=255)
+draw.text((x, top+20), 'World!', font=font, fill=255)
+
+# Display image.
+disp.image(image)
+disp.display()
