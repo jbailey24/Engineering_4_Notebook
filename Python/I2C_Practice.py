afa54c216c9b31a3011f346569e418092a18952d
jbailey24 <56133021+jbailey24@users.noreply.github.com>
2021-11-30 11:48:53 -0500

    Update I2C_Practice.py

--- Python/I2C_Practice.py
+++ Python/I2C_Practice.py
@@ -44,12 +44,15 @@ while True:
   accel_x, accel_y, accel_z = accel
   mag_x, mag_y, mag_z = mag
 
-  draw.text((x, top+12),    'X: '+accel_x,  font=font, fill=255)
-  draw.text((x, top+24), 'Y: '+accel_y, font=font, fill=255)
-  draw.text((x, top+36), 'Z: '+accel_z, font=font, fill=255)
+  draw.text((x, top+12),    'X: '+ str(accel_x),  font=font, fill=255)
+  draw.text((x, top+24), 'Y: '+ str(accel_y), font=font, fill=255)
+  draw.text((x, top+36), 'Z: '+ str(accel_z), font=font, fill=255)
 
   disp.image(image)
   disp.display()
 
   time.sleep(1)
+  
+  disp.clear()
+  
 
