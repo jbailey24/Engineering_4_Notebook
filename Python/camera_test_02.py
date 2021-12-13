3cbd08d8c4dbb84b68976502da379cba6578cd13
jbailey24 <56133021+jbailey24@users.noreply.github.com>
2021-12-13 12:56:32 -0500

    Create camera_test_02.py

---
+++ Python/camera_test_02.py
@@ -0,0 +1,26 @@
+
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.start_preview()
+    camera.image_effect = 'sketch'
+    time.sleep(2)
+    print('Say Cheese')
+    camera.capture('/pics/camera_code_2_sketch.jpg')
+    camera.image_effect = 'pastel'
+    time.sleep(1)
+    camera.capture('/pics/camera_code_2_pastel.jpg')
+    camera.image_effect = 'washedout'
+    time.sleep(1)
+    camera.capture('/pics/camera_code_2_washedout.jpg')
+    camera.image_effect = 'cartoon'
+    time.sleep(1)
+    camera.capture('/pics/camera_code_2_cartoon.jpg')
+    camera.image_effect = 'oilpaint'
+    time.sleep(1)
+    camera.capture('/pics/camera_code_2_oilpaint.jpg')
+    time.sleep(.5)
+    print('Done')
+    
