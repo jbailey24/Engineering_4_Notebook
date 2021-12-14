5783cd4459018980c39458623fffcb940e62562b
jbailey24 <56133021+jbailey24@users.noreply.github.com>
2021-12-14 12:22:30 -0500

    Update camera_test_02.py

--- Python/camera_test_02.py
+++ Python/camera_test_02.py
@@ -8,19 +8,19 @@ with picamera.PiCamera() as camera:
     camera.image_effect = 'sketch'
     time.sleep(2)
     print('Say Cheese')
-    camera.capture('/pics/camera_code_2_sketch.jpg')
+    camera.capture('/home/pi/Documents/Engineering_4_Notebook/Python/pics/camera_code_2_sketch.jpg')
     camera.image_effect = 'pastel'
     time.sleep(1)
-    camera.capture('/pics/camera_code_2_pastel.jpg')
+    camera.capture('/home/pi/Documents/Engineering_4_Notebook/Python/pics/camera_code_2_pastel.jpg')
     camera.image_effect = 'washedout'
     time.sleep(1)
-    camera.capture('/pics/camera_code_2_washedout.jpg')
+    camera.capture('/home/pi/Documents/Engineering_4_Notebook/Python/pics/camera_code_2_washedout.jpg')
     camera.image_effect = 'cartoon'
     time.sleep(1)
-    camera.capture('/pics/camera_code_2_cartoon.jpg')
+    camera.capture('/home/pi/Documents/Engineering_4_Notebook/Python/pics/camera_code_2_cartoon.jpg')
     camera.image_effect = 'oilpaint'
     time.sleep(1)
-    camera.capture('/pics/camera_code_2_oilpaint.jpg')
+    camera.capture('/home/pi/Documents/Engineering_4_Notebook/Python/pics/camera_code_2_oilpaint.jpg')
     time.sleep(.5)
     print('Done')
     
